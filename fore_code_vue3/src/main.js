ec6a256f21322df89695d3040a40fa7d33f30e02
13EG <126761670+13EG@users.noreply.github.com>
2024-01-02 19:41:52 +0800

    remove useless folder
    删除之前用vue3写的作废的项目文件

--- fore_code_vue3/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/Login.js'
-// import './plugins/element.js'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
