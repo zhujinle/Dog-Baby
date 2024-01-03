93af118878a99162052545d981eb8c99dc2d1828
13EG <126761670+13EG@users.noreply.github.com>
2024-01-04 00:30:51 +0800

    Login & (part of) Admin
    1.完成登录网页，但接口未调通
    2.完成后台管理系统的头部和侧边栏
    3.编辑了一份目前需调用到的接口的文档api.json

--- fore_code/src/components/Login.vue
+++ fore_code/src/components/Login.vue
@@ -3,22 +3,23 @@
 		<div class="login_box">
 			<!-- 头像区域 -->
 			<div class="avatar_box">
-				<img src="../assets/logo.png" alt="登录图像">
+				<img src="../assets/logo.png">
 			</div>
 			<!-- 表单区域 -->
-			<el-form label-width="0px">
+			<el-form ref="LoginFormRef" :model="LoginForm" :rules="LoginFormRules" label-width="0px" class="login_form">
 				<!-- 用户名 -->
-				<el-form-item>
-					<el-input></el-input>
+				<el-form-item prop="Username">
+					<el-input v-model="LoginForm.Username" prefix-icon="iconfont icon-yonghu" placeholder="用户名"></el-input>
 				</el-form-item>
 				<!-- 密码 -->
-				<el-form-item>
-					<el-input></el-input>
+				<el-form-item prop="Password">
+					<el-input v-model="LoginForm.Password" type="password" prefix-icon="iconfont icon-suoding" placeholder="密码"></el-input>
 				</el-form-item>
+				
 				<!-- 按钮区域 -->
-				<el-form-item>
-					<el-button type="login">登录</el-button>
-					<el-button type="info">重置</el-button>
+				<el-form-item class="btns">
+					<el-button type="primary" @click="Login()">登录</el-button>
+					<el-button type="info" @click="ResetLoginForm()">重置</el-button>
 				</el-form-item>
 			</el-form>
 		</div>
@@ -26,45 +27,103 @@
 </template>
 
 <script>
-export default {}
+export default {
+	data(){
+		return{
+			// 定义登录表单的数据绑定对象
+			LoginForm: {
+				Username:'',
+				Password:''
+			},
+			// 定义表单的验证规则对象
+			LoginFormRules: {
+				// 验证用户名是否合法
+				Username:[
+					{required:true, message:"请输入用户名称", trigger: "blur"},
+					{min:3, max:10, message:"长度在3-10个字符", trigger: "blur"}
+				],
+				// 验证密码是否合法
+				Password:[
+					{required:true, message:"请输入密码", trigger: "blur"},
+					{min:7, max:20, message:"长度在7-20个字符", trigger: "blur"}
+				]
+			}
+		};
+	},
+	methods: {
+		// 点击重置按钮，重置登录表单
+		ResetLoginForm(){
+			this.$refs.LoginFormRef.resetFields();
+		},
+		Login(){
+			this.$refs.LoginFormRef.validate(async valid =>{
+				if (!valid) return
+				        const params = new URLSearchParams()
+						// 根据API调整(已调，待测试)
+				        params.append('username', this.LoginForm.Username)
+				        params.append('password', this.LoginForm.Password)
+				        const res = await this.$http.post('Login', params)
+				        console.log(res)
+				        if (res.data.statusCode !== 200) return this.$message.error('res.data.msg')
+				        console.log(res)
+						
+						// 保存并上传token（根据API调整————已调，待测试）
+				        window.sessionStorage.setItem('token', res.data.token)
+				        this.$message.success('登录成功！')
+						
+						// 页面跳转
+				        this.$router.push('/admin')
+			})
+		}
+	}
+}
+	
 </script>
 
 <style lang="less" scoped>
 	.login_container{
-		background-color: #2b4b6b;
+		background-color: #434343;
 		height: 100%;
 	}
 	.login_box{
-		width: 450px;
-		height: 300px;
-		background-color: white;
-		border-radius: 3px;
+		width: 500px;
+		height: 350px;
+		background-color: #ffffff;
+		border-radius: 5px;
+		box-shadow: 10px 10px 10px 1px #8e8e8e;
 		position: absolute;
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
-	
-		.avatar_box{
-			width: 130px;
-			height: 130px;
-			border:1px solid #eee;
-			border-radius: 50%;
-			padding: 10px;
-			box-shadow: 0 0 10px #eee;
-			background-color: #fff;
-			img{
-				width: 100%;
-				height: 100%;
-				border-radius: 50%;
-				background-color: #eee;
-			}
-			position: absolute;
-			left: 50%;
-			transform: translate(-50%, -50%);
-		}
-	
-	
-	
 	}
-	
+	.avatar_box{
+		width: 110px;
+		height: 110px;
+		border:1px solid #eee;
+		border-radius: 50%;
+		padding: 7px;
+		box-shadow: 0 0 10px #eee;
+		background-color: #fff;
+		position: absolute;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+	.avatar_box img{
+		width: 100%;
+		height: 100%;
+		border-radius: 50%;
+		background-color: #eee;
+	}
+	.login_form{
+		position: absolute;
+		bottom: 5%;
+		width: 100%;
+		height: 200px;
+		padding: 0 20px;
+		box-sizing: border-box;
+	}
+	.btns{
+		display: flex;
+		justify-content: flex-end;
+	}
 </style>
--- fore_code/src/router/index.js
+++ fore_code/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/Login.vue'
+import Admin from '../components/Admin.vue'
 
 Vue.use(Router)
 
-
-export default new Router({
+const router = new Router({
 	routes: [
 	// 重定向网页，如下意思即为启动APP时，默认将网址定向到login页面
-		{ path: '/', redirect:'login'},
+		{ path: '/', redirect:'admin'},
     // 当用户在浏览器中访问/login'这个地址的时候，用component属性来展示Login组件
-		{ path: '/login', component: Login }
+		{ path: '/login', component: Login },
+		{ path: '/admin', component: Admin }
 	]
 })
+
+// 挂载路由导航守卫（这段代码会影响生产过程中检查别的组件，所以在项目完成前暂时先备注掉）
+// router.beforeEach((to, from, next) => {
+// 	if(to.path ==='/login') return next();
+// 	// 获取SecretKey
+// 	const SceretKey = window.sessionStorage.getItem('SecretKey')
+// 	  if (!SceretKey) {
+// 	    return next('/login')
+// 	  }
+// 	  next()
+// })
+export default router
